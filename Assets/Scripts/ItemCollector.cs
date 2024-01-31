787b38586c0b4551f68c7f0fea8c12adf74bb38f
unknown <gitrepowizard@gmail.com>
2024-01-31 08:16:44 -0600

    passed cherry count to next scene, made animation work with code

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -7,20 +7,27 @@ using UnityEngine;
 public class ItemCollector : MonoBehaviour
 {
 
-   private int numCherries = 0;
+   // private int numCherries = 0;
    [SerializeField] private AudioSource itemCollectSoundEffect;
    [SerializeField] private TextMeshProUGUI cherriesText;
+   //[SerializeField] private Animator cherryAnimator;
 
    private void OnTriggerEnter2D(Collider2D coll)
    {
         if(coll.gameObject.CompareTag("Cherry"))
         {
             //Debug.Log("Player Collected Cherry!");
-            Destroy(coll.gameObject);
+            Debug.Log("Collected Cherry");
+            
             //numCherries++;
             StateNameController.numCherries++;
             cherriesText.text = "Cherries: " + StateNameController.numCherries;
             itemCollectSoundEffect.Play();
+            coll.gameObject.GetComponent<Animator>().SetTrigger("collected");
+            //coll.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            coll.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            Debug.Log("Get collected trigger value: " + coll.gameObject.GetComponent<Animator>().GetBool("collected"));
+            //Destroy(coll.gameObject);
         }
    }
 }
---
+++ Assets/Scripts/StateNameController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int numCherries = 0;
+}
